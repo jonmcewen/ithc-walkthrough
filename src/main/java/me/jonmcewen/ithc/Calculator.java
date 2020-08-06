0c45e341aa9ddcbf900daa774ffaaf1fb0ffb340
Jon McEwen <jon.mcewen1@digital.homeoffice.gov.uk>
2020-08-06 15:43:46 +0100

    fix arithmetic

--- src/main/java/me/jonmcewen/ithc/Calculator.java
+++ src/main/java/me/jonmcewen/ithc/Calculator.java
@@ -1,8 +1,10 @@
 package me.jonmcewen.ithc;
 
+import static java.lang.Math.addExact;
+
 public class Calculator {
 
   public static int add(int x, int y) {
-    return x + y;
+    return addExact(x, y);
   }
 }
